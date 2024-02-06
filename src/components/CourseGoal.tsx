62e20c8e7bfabe78855fadac10a8b3a828c5c6bb
averageC0deEnjoyer <136006438+averageC0deEnjoyer@users.noreply.github.com>
2024-02-06 21:15:51 +0700

    add CourseGoal component

---
+++ src/components/CourseGoal.tsx
@@ -0,0 +1,13 @@
+const CourseGoal = () => {
+  return (
+    <article>
+      <div>
+        <h2>Title</h2>
+        <h2>Description</h2>
+      </div>
+      <button>DELETE</button>
+    </article>
+  );
+};
+
+export default CourseGoal;
